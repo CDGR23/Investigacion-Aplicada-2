b8daca0b0437def8adfa6cd7bee705d2f028dd13
César Guzmán GR220295 <151095877+CDGR23@users.noreply.github.com>
2024-09-11 20:34:42 -0600

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,13 +1,16 @@
 const jwt = require('jsonwebtoken');
 const Usuario = require('../models/Usuario');
 
+//Función para verificar el token proporcionado.
 const verificarToken = (req, res, next) => {
+  //Extrae el token de los encabezados.
   const token = req.headers['authorization'];
 
   if (!token) {
       return res.status(403).json({ mensaje: 'Token no proporcionado' });
   }
 
+  //Verifica la validez del token secreto definido en el archivo .env
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
       if (err) {
           return res.status(401).json({ mensaje: 'Token inválido o expirado' });
@@ -18,4 +21,5 @@ const verificarToken = (req, res, next) => {
   });
 };
 
+//Exporta la función para que se pueda usar en otros códigos.
 module.exports = verificarToken;
