44d766b0660008f774f4599f791dbfad89789504
César Guzmán GR220295 <151095877+CDGR23@users.noreply.github.com>
2024-09-11 20:43:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,15 @@ const conexion = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const dotenv = require('dotenv');
 
-dotenv.config();
-const app = express();
+dotenv.config(); //Carga las variables de entorno del archivo .env
+const app = express(); //Crea una instancia de la aplicación Express.
 app.use(express.json());
 
-app.use('/api', authRoutes);
+app.use('/api', authRoutes); //Usa las rutas bajo el prefijo de /api.
 
 const PORT = process.env.PORT || 4000;
+
+//Inicia el servidor en el puerto especificado.
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
