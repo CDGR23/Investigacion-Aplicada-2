a904c5af519b394e7d492e05f36e18b08173e434
César Guzmán GR220295 <151095877+CDGR23@users.noreply.github.com>
2024-09-11 20:40:45 -0600

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,9 +3,17 @@ const controlador = require('../controllers/authController');
 const verificarToken = require('../middleware/authMiddleware');
 const router = express.Router();
 
+//Ruta para registrar un nuevo usuario.
 router.post('/registro', controlador.registrarUsuario);
+
+//Ruta para iniciar sesión.
 router.post('/login', controlador.iniciarSesion);
+
+//Ruta para acceder a un recurso protegido.
 router.get('/recursoprotegido', verificarToken, controlador.recursoProtegido);
+
+//Ruta para cerrar sesión.
 router.post('/logout', verificarToken, controlador.cerrarSesion);
 
+//Exporta el enrutador para que se pueda usar en otros códigos.
 module.exports = router;
